95c4b35c515d7fa1d571d74e7d1e1212238d97fe
Kyra Paraiso <24mashpotato@gmail.com>
2025-11-02 20:53:38 +0800

    adjusted daily faction ball reset to est time

--- utils/background_task/scheduler.py
+++ utils/background_task/scheduler.py
@@ -1,7 +1,14 @@
-from utils.essentials.schedule_manager import SchedulerManager
+import zoneinfo
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
 from utils.background_task.daily_fa_ball_reset import daily_faction_ball_reset
+from utils.essentials.schedule_manager import SchedulerManager
 from utils.logs.pretty_log import pretty_log
+
 scheduler_manager = SchedulerManager(timezone_str="Asia/Manila")
+NYC = zoneinfo.ZoneInfo("America/New_York")  # auto-handles EST/EDT
+
 
 # 🌙───────────────────────────────────────────────🌙
 #           Background Task Scheduler Setup
@@ -9,17 +16,18 @@ scheduler_manager = SchedulerManager(timezone_str="Asia/Manila")
 async def setup_scheduler(bot):
     """Setup and start the scheduler with background tasks."""
 
-    # Daily Faction Ball Reset at 12 pm Manila
+    # Daily Faction Ball Reset at 12 AM EST
     scheduler_manager.add_cron_job(
         daily_faction_ball_reset,
         name="daily_faction_ball_reset",
-        hour=12,
+        hour=0,
         minute=0,
         args=[bot],
+        timezone=NYC,
     )
     pretty_log(
         tag="background_task",
-        message="Scheduled daily faction ball reset at 12:00 PM Manila time.",
+        message="Scheduled daily faction ball reset at 12:00 AM EST.",
         bot=bot,
     )
 
